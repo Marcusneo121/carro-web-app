2ba8bc07b9ceb19d5f11d859aad0fd5c9a9b6a2c
Marcus Neo <marcus121neo@gmail.com>
2024-04-16 22:02:36 +0800

    setup host car page

---
+++ app/(host)/hostcar/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HostPage = () => {
+  return (
+    <div className="min-h-[800px] items-center px-8 py-10 lg:px-20">
+      HostPage
+    </div>
+  );
+};
+
+export default HostPage;
